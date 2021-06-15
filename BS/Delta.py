48c2b5bfba8ceca0a9035f8fd9261e0965ed82ed
TheGabrielRodriguez <81578746+TheGabrielRodriguez@users.noreply.github.com>
2021-06-14 20:51:28 -0700

    Delete BS directory

--- BS/Delta.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy.stats import norm
-
-def delta(flag, s, k, t, r, v):
-    d1 = (np.log(s/k)+(r+v*v/2)*t)/(v*np.sqrt(t))
-    if flag == "C":
-        return norm.cdf(d1)
-    else:
-        return norm.cdf(-d1)
